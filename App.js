dc655cb4f9cae491d3697338cef01f105f6bf558
Nour Hellou <noura-pretty17@hotmail.com>
2018-08-15 01:53:21 +0300

    react-native-facebook login succeed with firebase integration

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import firebase from 'react-native-firebase';
 
 const FBSDK = require('react-native-fbsdk');
 
-const {LoginButton,AccessToken} = FBSDK;
+const {LoginButton,AccessToken,LoginManager} = FBSDK;
+
 
 
 
@@ -98,7 +99,31 @@ if (error) {
     (data) => {
 
       console.log(data);
-      onLoginOrRegister();
+      LoginManager.logInWithReadPermissions(['public_profile', 'email'])
+      .then((result) => {
+        if (result.isCancelled) {
+          return Promise.reject(new Error('The user cancelled the request'));
+        }
+        // Retrieve the access token
+        return AccessToken.getCurrentAccessToken();
+      })
+      .then((data) => {
+        // Create a new Firebase credential with the token
+        const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+        // Login with the credential
+        return firebase.auth().signInWithCredential(credential);
+      })
+      .then((user) => {
+        // If you need to do anything with the user, do it here
+        // The user will be logged in automatically by the
+        // `onAuthStateChanged` listener we set up in App.js earlier
+      })
+      .catch((error) => {
+        const { code, message } = error;
+        // For details of error codes, see the docs
+        // The message contains the default Firebase string
+        // representation of the error
+      });
       
 
     }
